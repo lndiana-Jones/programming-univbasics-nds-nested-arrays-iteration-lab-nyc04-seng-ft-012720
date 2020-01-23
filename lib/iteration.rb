a02ff6ebdb4c797aa0f2c2e9b9a85ae235ecd2f8
Andrew D'Antonio <andrewdantonio10@gmail.com>
2020-01-23 22:06:59 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,57 @@
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  outer_arr = []
+  row_index = 0 
+  while row_index < src.length do
+    element_index = 0 
+    inner_arr = []
+    while element_index <= 1 do
+      inner_arr << src[row_index][element_index]
+      element_index += 1
+    end
+    row_index += 1 
+    outer_arr << "I love #{inner_arr[0]} and #{inner_arr[1]} on my pizza"
+  end 
+  
+  outer_arr
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+row_index = 0 
+  outer_arr = []
+  
+  while row_index < src.count do
+    element_index = 0
+    inner_arr = []
+    while element_index < src[row_index].count do
+      inner_arr << src[row_index][element_index]
+      end   
+    if inner_arr[0] < inner_arr[1]
+      inner_arr.shift
+    else
+      inner_arr.pop
+    end 
+    row_index += 1 
+    outer_arr << inner_arr
+  end
+  outer_arr
+
 end
 
+
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  outer_arr = []
+  row_index = 0 
+  while row_index < src.length
+    element_index = 0 
+    inner_arr = []
+    while element_index < src[row_index].length
+      inner_arr << src[row_index][element_index]
+    end
+  if (inner_arr[0] % 2 == 0) && (inner_arr[1] % 2 == 0)
+    outer_arr << inner_arr
+  end
+  row_index += 1 
+  end
+  
+  outer_arr
 end
